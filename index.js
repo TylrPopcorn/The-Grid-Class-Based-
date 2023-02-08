3f411eaffc4b3cca67b332b2f280ee9f7da5ec56
TylrPopcorn <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2023-02-08 07:36:53 -0500

    Added redux access

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./src/App";
-
+//REDUCER STUFF: --
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import Reducer from "./state/Reducer";
+//--
 import "./styles/reset.css";
 import "./styles/styles.css";
 
+import App from "./src/App";
+//-----------------------------------
 /**
  *  Dependencies:
  *
  *  npm i pacel-bundler
  *  npm i react
- *
+ *  npm i redux
+ *  npm i react-redux
  */
+
+// REDUX STORE
+let store;
+export const resetStore = () => {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  store = createStore(Reducer, composeEnhancers(applyMiddleware(thunk)));
+};
+resetStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <h1>Welcome to the GRID</h1>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
